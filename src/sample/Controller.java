bdc2598c8ab6db947a7378e8f9b4fea8580e5517
Elena <alenatitova99@mail.ru>
2022-01-11 22:31:32 +0400

    LR1_V2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,55 +1,133 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
     @FXML
-    CheckBox checkBox1 = new CheckBox();
+    private CheckBox C = new CheckBox();
     @FXML
-    CheckBox checkBox2 = new CheckBox();
+    private CheckBox S = new CheckBox();
     @FXML
-    CheckBox checkBox3 = new CheckBox();
+    private CheckBox R = new CheckBox();
     @FXML
-    ImageView imageView = new ImageView();
+    private ImageView imageView = new ImageView();
+    @FXML
+    private TextArea textArea = new TextArea();
+    @FXML
+    private Button button = new Button();
+    @FXML
+    private TextArea textArea2 = new TextArea();
 
-    Class<?> clazz = this.getClass();
+    private int Q = 0;
+    private boolean remembered = false;
+    private Class<?> clazz = this.getClass();
+
+    private String path = "";
 
     public void onClickMethod(ActionEvent actionEvent) {
-
-        checkBox1.setOnAction(event -> show());
-        checkBox2.setOnAction(event -> show());
-        checkBox3.setOnAction(event -> show());
-
-
+        C.setOnAction(event -> show());
+        S.setOnAction(event -> show());
+        R.setOnAction(event -> show());
     }
-    String path = "";
-    private void show() {
 
-        if(checkBox1.isSelected()){
-            path = "/ImagePackage/C0R0S0Q0.jpg";
+    private void show() {
+        textArea.clear();
+        if(C.isSelected()){
+            if (!S.isSelected() && !R.isSelected() && !remembered){
+                path = "/ImagePackage/C1SXRXQX.jpg";
+                Q = 0;
+            }
+            else if(S.isSelected() && !R.isSelected() && (Q == 0)){
+                path = "/ImagePackage/C1S1R0Q0.jpg";
+                Q = 1;
+                remembered = true;
+            }
+            else if(S.isSelected() && !R.isSelected() && (Q == 1)){
+                path = "/ImagePackage/C1S1R0Q1.jpg";
+                Q = 1;
+                remembered = true;
+            }
+            else if(!S.isSelected() && R.isSelected() && (Q == 1)){
+                path = "/ImagePackage/C1S0R1Q1.jpg";
+                Q = 0;
+                remembered = true;
+            }
+            else if(!S.isSelected() && R.isSelected() && (Q == 0)){
+                path = "/ImagePackage/C1S0R1Q0.jpg";
+                Q = 0;
+                remembered = true;
+            }
+            else if(!S.isSelected() && !R.isSelected() && (Q == 1)){
+                path = "/ImagePackage/C1S0R0Q1.jpg";
+                Q = 1;
+                remembered = true;
+            }
+            else if(!S.isSelected() && !R.isSelected() && (Q == 0)){
+                path = "/ImagePackage/C1S0R0Q0.jpg";
+                Q = 0;
+                remembered = true;
+            }
+            else if(S.isSelected() && R.isSelected()){
+                textArea.setText("Не определено");
+                path = "/ImagePackage/C0S0R0Q0.jpg";
+                Q = 0;
+                remembered = false;
+            }
+        }else{
+            if (!remembered){
+                path = "/ImagePackage/C0S0R0Q0.jpg";
+            }else if (S.isSelected() && R.isSelected()){
+                path = "/ImagePackage/C0S0R0Q0.jpg";
+                Q = 0;
+                remembered = false;
+            }
+            else if(Q == 1){
+                path = "/ImagePackage/C0SXRXQ1.jpg";
+            }else if (Q == 0){
+                path = "/ImagePackage/C0SXRXQ0.jpg";
+            }
+
         }
-        if(checkBox2.isSelected()){
-            path = "/ImagePackage/C0R0S0Q0.jpg";
-        }
-        if(checkBox3.isSelected()){
-            path = "/ImagePackage/C1R0S0Q0.jpg";
-        }
+
         InputStream input = clazz.getResourceAsStream(path);
         Image image = new Image(input);
         imageView.setImage(image);
     }
 
-
-
-
-
-
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        button.setStyle("-fx-font: 16 arial; -fx-base: black;");
+        textArea.setStyle("-fx-background-color: black; -fx-font: 16 arial;");
+        C.setStyle("-fx-font: 16 arial;");
+        S.setStyle("-fx-font: 16 arial;");
+        R.setStyle("-fx-font: 16 arial;");
+        textArea2.setStyle("-fx-background-color: black; -fx-font: 14 arial;");
+        textArea2.setText("В данной работе представлена визуализация синхронного RS-триггера." +
+                "\nДля запуска нажмите кнопку 'Start'. " +
+                "\nДля изменения состояния триггера переключите входные сигналы C, S или R.");
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                button.setText("Started");
+            }
+        });
+        InputStream input = clazz.getResourceAsStream("/ImagePackage/C0S0R0Q0.jpg");
+        Image image = new Image(input);
+        imageView.setImage(image);
+    }
 }
